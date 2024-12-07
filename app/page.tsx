9786dac68fd72818163dcf0ca2b71b7c3b6cdcca
66026145 <154246515+66026145@users.noreply.github.com>
2024-12-07 21:23:16 +0700

    ข้อความอธิบายการเปลี่ยนแปลง

--- app/page.tsx
+++ app/page.tsx
@@ -4,32 +4,31 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <div className="font-sans p-8 bg-gradient-to-r from-orange-500 via-red-600 to-blue-800 text-gray-900 min-h-screen">
+    <div className="font-sans p-8 bg-gradient-to-r from-indigo-950 via-teal-500 to-violet-950 text-gray-900 min-h-screen">
       {/* Header Section */}
       <header className="text-center mb-10">
-        <h1 className="text-5xl font-bold mb-2 text-white">อุจิวะ ซาสึเกะ</h1>
-        <p className="text-xl italic text-gray-200">"อย่างนาย จะไปรู้อะไร"</p>
+        <h1 className="text-5xl font-bold mb-2 text-indigo-900">อุจิวะ ซาสึเกะ</h1>
+        <p className="text-xl italic  text-gray-100">
+        " อย่างนาย..จะไปรู้อะไร!!"        </p>
       </header>
 
       {/* About Section */}
-      <section className="mb-12">
-        {/* Profile Image */}
-        <img
-          src="https://cdn.readawrite.com/articles/8049/8048121/thumbnail/large.gif?2"
-          alt="Profile"
-          className="mx-auto rounded-full w-36 h-36 mb-6 transform hover:scale-105 transition-transform duration-300"
-        />
-        <h2 className="text-3xl font-semibold mb-4 text-indigo-200">แนะนำตัวแบบสั้นๆ</h2>
-        <p className="text-xl text-gray-100 leading-relaxed">
-          ฉันชื่อ อุจิวะ ซาสึเกะ ของที่เกลียดมีเยอะแยะ ส่วนของที่ชอบไม่มีสักอย่าง
-          เป้าหมายของฉันจะไม่เป็นเพียงความฝัน มันจะต้องกลายเป็นจริง เพราะฉันจะฟื้นฟูตระกูล
-          และฆ่าชายคนหนึ่งให้ได้
-        </p>
-      </section>
+<section className="mb-12">
+  {/* Profile Image */}
+  <img
+    src="https://cdn.readawrite.com/articles/8049/8048121/thumbnail/large.gif?2"
+    alt="Profile"
+    className="mx-auto rounded-full w-49 h-42 mb-6 transform hover:scale-105 transition-transform duration-300"
+  />
+  <h2 className="text-3xl font-semibold mb-4 text-indigo-300">แนะนำตัวแบบสั้นๆ</h2>
+  <p className="text-xl text-gray-100 leading-relaxed">
+  ฉันชื่อ อุจิวะ ซาสึเกะ ของที่เกลียดมีเยอะแยะ ส่วนของที่ชอบไม่มีสักอย่าง เป้าหมายของฉันจะไม่เป็นเพียงความฝันมันจะต้องกลายเป็นจริง เพราะฉันจะฟื้นฟูตระกูลและฆ่าชายคนหนึ่งให้ได้
+  </p>
+</section>
 
       {/* Skills Section */}
       <section className="mb-12">
-        <h2 className="text-3xl font-semibold mb-4 text-indigo-200">สกิลสุดหล่อเท่</h2>
+        <h2 className="text-3xl font-semibold mb-4 text-indigo-300">สกิลสสุดหล่อเท่</h2>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {["เนตรสังเวียน", "เนตรวงแหวนแมนเงเคียว", "ลูกศรของอินดรา", "รินเนกัง", "จักระอันมืดมิด", "เคนจูสึสไตล์อุจิวะ"].map(
             (skill, index) => (
@@ -44,29 +43,71 @@ const Portfolio = () => {
         </div>
       </section>
 
-      {/* Projects Section */}
-      <section className="mb-16">
-        <h2 className="text-3xl font-semibold mb-4 text-indigo-200">ความสำเร็จอันเกรียงไกร</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {/* Project Details */}
-          {/* ... */}
-        </div>
-      </section>
+      
+        {/* Projects Section */}
+        <section className="mb-16">
+          <h2 className="text-3xl font-semibold mb-4 text-indigo-300">ความสำเร็จอันเกรียงไกร</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-cyan-300 transform hover:scale-105">
+              <h3 className="text-2xl font-bold mb-3 text-indigo-700">
+                ยอมรับอักขระต้องสาป
+              </h3>
+              <p className="text-gray-700">
+              เดินทางไปฝึกฝนศาสตร์มืดกับโอโรจิมารุศัตรูคนสำคัญของหมู่บ้านโคโนฮะ 
+              เขายอมทิ้งทุกอย่างในชีวิตเพียงแลกกับหนทางใกล้สุดที่จะทำให้เขาทำตามเป้าหมายที่วางเอาไว้ได้
+              </p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-cyan-300 transform hover:scale-105">
+              <h3 className="text-2xl font-bold mb-3 text-indigo-700">
+                ฆ่าพี่ชาย
+              </h3>
+              <p className="text-gray-700">
+              การ ‘ดีดหน้าผาก’ ครั้งสุดท้าย และประโยคสุดท้าย “ขอโทษนะซาสึเกะ…นี่คงเป็นครั้งสุดท้ายแล้ว” 
+              </p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-cyan-300 transform hover:scale-105">
+              <h3 className="text-2xl font-bold mb-3 text-indigo-700">
+              สงครามโลกนินจาครั้ง 4 
+              </h3>
+              <p className="text-gray-700">
+              ผนึกกำลังร่วมกับนารูโตะและเพื่อนๆ ต่อสู้กับลาสต์บอสอย่าง อุจิวะ มาดาระ และคางูยะ
+              </p>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-cyan-300 transform hover:scale-105">
+              <h3 className="text-2xl font-bold mb-3 text-indigo-700">
+                แต่งงานกับซากุระ
+              </h3>
+              <p className="text-gray-700">
+              ปากบอกไม่ชอบ แต่แต่งกับเขาเฉย งง
+              </p>
+            </div>
+          </div>
+        </section>
 
       {/* Contact Section */}
       <section>
-        <h2 className="text-3xl font-semibold mb-4 text-indigo-200">ข้อมูลติดต่อ</h2>
-        <p className="text-gray-100 text-xl">ส่งอีเมลล์มาไม่ตอบกลับเพราะนินจาติดต่อผ่านการส่งจดหมาย</p>
-        <ul className="list-none mt-4">
+        <h2 className="text-3xl font-semibold mb-4 text-indigo-300">
+          ข้อมูลติดต่อ
+        </h2>
+        <p className="text-gray-100 text-xl">
+          ส่งอีเมลล์มาไม่ตอบกลับเพราะนินจาติดต่อผ่านการส่งจดหมาย
+        </p>
+        <ul className="list-none mt-4 text-indigo-500">
           <li>
             <strong>Email:</strong>{" "}
-            <a href="mailto:66026145@up.ac.th" className="text-indigo-200 hover:underline">
+            <a
+              href="mailto:66026145@up.ac.th"
+              className="text-indigo-200 hover:underline"
+            >
               66026145@up.ac.th
             </a>
           </li>
           <li>
             <strong>Phone:</strong>{" "}
-            <a href="tel:012-345-6789" className="text-indigo-200 hover:underline">
+            <a
+              href="tel:012-345-6789"
+              className="text-indigo-200 hover:underline"
+            >
               012-345-6789
             </a>
           </li>
---
+++ h
@@ -0,0 +1,56 @@
+[33mcommit 9e0b25cc9ecff4b69a997bfa3e2b3af3cb1c876b[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Dec 7 20:41:30 2024 +0700
+
+    บันทึกการเปลี่ยนแปลงก่อน revert
+
+[33mcommit cb914dba798c72973bb4c3fc671b102ec634bd92[m
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Dec 7 19:46:19 2024 +0700
+
+    ข้อความอธิบาย
+
+[33mcommit 37fc051b0248dacf8ad63225ff9cfa210ad3d320[m
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Dec 7 19:38:59 2024 +0700
+
+    ข้อความอธิบาย
+
+[33mcommit 143beede68d4dfd85d269525cca7771a93e4439e[m
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Dec 7 19:25:58 2024 +0700
+
+     bg
+
+[33mcommit 994343f65c692f9e18dd039605b2378c6afe3764[m
+Merge: d73c2e4 b661a50
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Dec 7 18:45:27 2024 +0700
+
+    Merge pull request #1 from 66026145/feature-branch
+    
+    Feature branch
+
+[33mcommit b661a50e243e9ef69dd5278f5b13f9e2877818fd[m[33m ([m[1;31morigin/feature-branch[m[33m, [m[1;32mfeature-branch[m[33m)[m
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Dec 7 18:43:05 2024 +0700
+
+    fix
+
+[33mcommit 4f1c51b5329df0ffbd365c2713ad6f32d14cd877[m[33m ([m[1;31morigin/new-branch-name[m[33m, [m[1;32mnew-branch-name[m[33m)[m
+Author: 66026145 <154246515+66026145@users.noreply.github.com>
+Date:   Sat Nov 30 21:47:13 2024 +0700
+
+    ข้อความที่เกี่ยวข้องกับการเปลี่ยนแปลง
+
+[33mcommit d73c2e4a682e74a146962a1efbc7d0a9c385dc0b[m
+Author: natpaphat <66026145@up.ac.th>
+Date:   Tue Nov 19 15:02:36 2024 +0700
+
+    Updated resume page
+
+[33mcommit ccd01b03612fb835695786510a274b6890563ab5[m
+Author: natpaphat <66026145@up.ac.th>
+Date:   Tue Nov 19 14:21:12 2024 +0700
+
+    Initial commit from Create Next App
